a44c4be7fe12809ef241645cd390c2e687cfed86
Kill3rbee <ldmw2k-github@yahoo.com>
2019-12-09 22:04:51 -0600

    Update multiple file

--- download-files.go
+++ download-files.go
@@ -9,52 +9,115 @@ import (
 	"os"
 	"strings"
 	"os/exec"
+	"path/filepath"
 )
 
 // DownloadFile will download a url to a local file. It's efficient because it will
 // write as it downloads and not load the whole file into memory.
-func downloadFromUrl(url string) {
+func downloadFromUrl(dirName string, url string) {
 	tokens := strings.Split(url, "/")
 	fileName := tokens[len(tokens)-1]
 	fmt.Println("Downloading", url, "to", fileName)
 
-	// TODO: check file existence first with io.IsExist
-	output, err := os.Create(fileName)
-	if err != nil {
-		fmt.Println("Error while creating", fileName, "-", err)
-		return
-	}
-	defer output.Close()
+	// Call create-directory microservice
+    if dirName != "" {
+		path := filepath.Join(dirName, fileName)
+		output, err := os.Create(path)
+		
+		if err != nil {
+			fmt.Println("Error while creating", fileName, "-", err)
+			return
+		}
+		defer output.Close()
 
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error while downloading", url, "-", err)
-		return
-	}
-	defer response.Body.Close()
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Println("Error while downloading", url, "-", err)
+			return
+		}
+		defer response.Body.Close()
 
-	_, err = io.Copy(output, response.Body)
-	if err != nil {
-		fmt.Println("Error while downloading", url, "-", err)
-		return
-	}
+		_, err = io.Copy(output, response.Body)
+		if err != nil {
+			fmt.Println("Error while downloading", url, "-", err)
+			return
+		}
 
-	//fmt.Println(n, "bytes downloaded.")
+    } else {
+		output, err := os.Create(fileName)
+		if err != nil {
+			fmt.Println("Error while creating", fileName, "-", err)
+			return
+		}
+		defer output.Close()
+
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Println("Error while downloading", url, "-", err)
+			return
+		}
+		defer response.Body.Close()
+
+		_, err = io.Copy(output, response.Body)
+		if err != nil {
+			fmt.Println("Error while downloading", url, "-", err)
+			return
+		}
+    }
 }
 
-func main() {
-	// Call microservice for finding links on page
-    args := "../bin/find_links_in_page"
-    cmd := exec.Command(args)
+// func createDirectory(dirName string) bool {
+// 	src, err := os.Stat(dirName)
 
-    stderr, _ := cmd.StdoutPipe()
+// 	if os.IsNotExist(err) {
+// 			errDir := os.MkdirAll(dirName, os.ModePerm)
+// 			if errDir != nil {
+// 					panic(err)
+// 			}
+// 			return true
+// 	}
+
+// 	if src.Mode().IsRegular() {
+// 			fmt.Println(dirName, "already exist as a file!")
+// 			return false
+// 	}
+
+// 	return false
+// }
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage : %s <directory>\n", os.Args[0])
+		os.Exit(0)
+	}
+
+	directory := os.Args[1]
+
+	// Call microservice for creating directory
+	dirService := "../../bin/create-directory"
+	args := []string{directory}
+
+	result := exec.Command(dirService, args...)
+
+	err := result.Start()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Command executed : ", result)
+
+	// Call microservice for finding links on page
+    linksService := "../../bin/find_links_in_page"
+    cmd := exec.Command(linksService)
+
+    stdout, _ := cmd.StdoutPipe()
     cmd.Start()
 
-    scanner := bufio.NewScanner(stderr)
+    scanner := bufio.NewScanner(stdout)
     for scanner.Scan() {
 		m := scanner.Text()
 		url := "https://nvd.nist.gov/feeds/json/cve/1.1/" + m
-		downloadFromUrl(url)
+		downloadFromUrl(directory, url)
     }
     cmd.Wait()
 }
