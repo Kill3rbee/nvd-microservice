508747c8e3693b94d30f1916820c3442959e98da
Kill3rbee <ldmw2k-github@yahoo.com>
2019-12-10 11:29:53 -0600

    Updated so it can take args

--- create-directory.go
+++ create-directory.go
@@ -2,33 +2,32 @@
 package main
 
 import (
-		"fmt"
-		"os"
+	"fmt"
+	"os"
 )
 
 func createDirectory(dirName string) bool {
-		src, err := os.Stat(dirName)
+	src, err := os.Stat(dirName)
 
-		if os.IsNotExist(err) {
-				errDir := os.MkdirAll(dirName, os.ModePerm)
-				if errDir != nil {
-						panic(err)
-				}
-				return true
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(dirName, os.ModePerm)
+		if errDir != nil {
+			panic(err)
 		}
+		return true
+	}
 
-		if src.Mode().IsRegular() {
-				fmt.Println(dirName, "already exist as a file!")
-				return false
-		}
-
+	if src.Mode().IsRegular() {
+		fmt.Println(dirName, "already exist as a file!")
 		return false
+	}
+	return false
 }
 
 func main() {
 	if len(os.Args) != 2 {
-			fmt.Printf("Usage : %s <directory>\n", os.Args[0])
-			os.Exit(0)
+		fmt.Printf("Usage : %s <directory>\n", os.Args[0])
+		os.Exit(0)
 	}
 
 	directory := os.Args[1]
